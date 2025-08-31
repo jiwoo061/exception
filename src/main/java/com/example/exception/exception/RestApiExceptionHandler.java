21785b3725bc2f38c448ca1d41329692e7eb2cf1
jyu <jiwoo061@gmail.com>
2025-08-31 18:21:12 +0900

    project init

---
+++ src/main/java/com/example/exception/exception/RestApiExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.example.exception.exception;
+
+import com.example.exception.controller.RestApiBController;
+import com.example.exception.controller.RestApiController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@ControllerAdvice
+// 글로벌하게 컨트롤러 어드바이스 만들기
+// @RestControllerAdvice(basePackages = "com.example.exception.controller")
+// @RestControllerAdvice(basePackageClasses =  {RestApiBController.class, RestApiController.class})
+public class RestApiExceptionHandler {
+
+    @ExceptionHandler(value={Exception.class})
+    public ResponseEntity exception(Exception e){
+        log.error("RestApiExceptionHandler", e);
+        return ResponseEntity.status(200).build();
+    }
+
+    @ExceptionHandler(value = {IndexOutOfBoundsException.class})
+    public ResponseEntity outOfBound(IndexOutOfBoundsException e){
+        log.error("IndexOutOfBoundsException", e);
+        return ResponseEntity.status(200).build();
+    }
+}
